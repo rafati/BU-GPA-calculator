ce6a96d0dc945fed467867d1cea3c6f95b00cd3a
rafati <rafat.ishaq@gmail.com>
2025-04-03 13:02:44 +0300

    Add Vercel configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production'
+  },
+  headers: async () => [
+    {
+      source: '/:path*',
+      headers: [
+        {
+          key: 'X-Frame-Options',
+          value: 'DENY'
+        },
+        {
+          key: 'X-Content-Type-Options',
+          value: 'nosniff'
+        },
+        {
+          key: 'Strict-Transport-Security',
+          value: 'max-age=31536000; includeSubDomains'
+        }
+      ]
+    }
+  ]
 };
 
 export default nextConfig;
